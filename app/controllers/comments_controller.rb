7791b183ccfe918d0c77c645d7a4d596283a503b
yutaka-sannomiya <yutakasannnomiya@gmail.com>
2021-04-17 15:57:32 +0900

    修正後

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
-      render "prptotypes/show" 
+      render "prototypes/show" 
     end
   end
 
